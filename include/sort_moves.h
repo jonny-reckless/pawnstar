c51eef2e61433cf7bf643a2fd84bb9cfbc8a128b
Jonny Reckless <jonny@recklesstechnology.com>
2024-07-19 09:23:32 -0700

    Formatting.

--- include/sort_moves.h
+++ include/sort_moves.h
@@ -10,16 +10,15 @@ void     ResetKillerCounts();
 void     ScoreAndSortMoves(const Position &position, MoveList &moves, int ply, int depth);
 uint32_t MaxKillerMoveCount(void);
 
-/**
- * @brief Sort moves "best first" i.e. in descending score order
- * @param moves Moves to be sorted
- * @param is_stable_sort true for slower stable sort (required at root node search only)
- */
+/// @brief Sort moves best first i.e. in descending score order
+/// @param moves Moves to be sorted
+/// @param is_stable_sort true for slower stable sort (required at root node search only)
 template <bool is_stable_sort> void SortMoves(MoveList &moves)
 {
     if constexpr (is_stable_sort)
     {
-        std::stable_sort(moves.begin(), moves.end(), [](const Move &a, const Move &b) { return a.score() > b.score(); });
+        std::stable_sort(moves.begin(), moves.end(),
+                         [](const Move &a, const Move &b) { return a.score() > b.score(); });
     }
     else
     {
